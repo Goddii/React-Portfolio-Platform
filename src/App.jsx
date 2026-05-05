146dc8c2a684a7145e8e5256cfba2aea4d36ae93
Goddii <otienogodwin23@gmail.com>
2026-05-05 11:41:06 +0300

    feat: add header for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import AddProject from './components/AddProject'
+import LandingPage from './components/LandingPage'
 
 function App() {
   
   return (
     <div>
+      <LandingPage />
       <AddProject />
     </div>
   )
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+
+
+function LandingPage(){
+    return (
+        <h1>Personal Project Showcase App</h1>
+    )
+}
+
+export default LandingPage;
