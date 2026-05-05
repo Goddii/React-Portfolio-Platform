8be61df13ef7a8752567ded9e9fc35635bd1d9be
Goddii <otienogodwin23@gmail.com>
2026-05-05 23:46:23 +0300

    style: style project title and description

--- src/components/SearchProject.jsx
+++ src/components/SearchProject.jsx
@@ -16,8 +16,8 @@ function SearchProject({projects}){
                 <p className='italic text-slate-400 animate-pulse mt-2 p-2 text-pretty'>No projects yet.Add one above</p>
             ):(<ul>
                 {filtered.map(project => (<li className="p-4 mb-3 mt-3 rounded-xl bg-white/10 border border-white/20 hover:border-blue-500/50 transition-colors " key={project.id}>
-                    <strong className="text-blue-400">{project.title}</strong>
-                    <p className="text-pretty">{project.description}</p>
+                    <strong className="text-cyan-400">{project.title}</strong>
+                    <p className="text-pretty" className='text-slate-300' text>{project.description}</p>
                 </li>))}
             </ul>)}
         </div>
